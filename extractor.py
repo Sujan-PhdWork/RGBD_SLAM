ea7823bfdd7af2b0f19c78abf4a40573dfaee809
Sujan-PhdWork <104067836+Sujan-PhdWork@users.noreply.github.com>
2023-12-29 19:19:06 +0530

    transformation is working based on two point pattern

--- extractor.py
+++ extractor.py
@@ -4,8 +4,9 @@ import numpy as np
 class Extractor():
     def __init__(self):
         self.orb=cv2.ORB_create(100)
-        # self.bf=cv2.BFMatcher()
+        self.bf=cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last=None
+
     def extract(self,img):
         #detection
         feats=cv2.goodFeaturesToTrack(np.mean(img,axis=2).astype(np.uint8),3000,qualityLevel=0.01,minDistance=3)
@@ -13,9 +14,16 @@ class Extractor():
         #extraction
         kps=[cv2.KeyPoint(x=f[0][0],y=f[0][1],size=20) for f in feats]
         kps,des=self.orb.compute(img,kps)
-        # matches=None
-        # #matching
-        # if self.last is not None:
-        #      matches=self.bf.match(des,self.last['des'])
+        matches=None
+        #matching
+        ret=[]
+        if self.last is not None:
+            matches=self.bf.knnMatch(des,self.last['des'],k=2)
+            for m,n in matches:
+                if m.distance<0.75*n.distance:
+                    kp1=kps[m.queryIdx].pt
+                    kp2=self.last['kps'][m.trainIdx].pt
+                    ret.append((kp1,kp2))
+            
         self.last={'kps':kps,'des':des}
-        return kps,des
+        return ret
--- utils.py
+++ utils.py
@@ -31,3 +31,4 @@ def data(file_path):
     return (file_list)
 
 
+
