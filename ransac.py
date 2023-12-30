77a1fab030f6b28e99830a529a1b9c5c16f48e88
Sujan-PhdWork <104067836+Sujan-PhdWork@users.noreply.github.com>
2023-12-30 12:02:17 +0530

    ransac and transformation working

--- ransac.py
+++ ransac.py
@@ -1,33 +1,42 @@
+import numpy as np 
+
+
 class RANSAC:
-    def __init__(self,matches,model,n,th,eta=0.3):
-        self.pt1=pt1 #need correction
-        self.pt2=pt2 
+    def __init__(self,matches,model,n,th,iter):
+        coordinate_array=np.array(matches)
+        self.kp1=coordinate_array[:,0,:].T
+        self.kp2=coordinate_array[:,1,:].T
+        
         self.sample=n
         self.threshold=th
-        self.eta=eta
+        self.iter=iter
         self.model=model
+        
     
     def solve(self):
         i=0
         best_inlier=0
-        T=(1-self.eta)*self.pt1.shape[1]
+        best_params={}
+        # T=(1-self.eta)*self.pt1.shape[1]
         # print(T)
-        while(i<100):
+        while(i<self.iter):
 
             #select random sample
             # print(self.pt1.shape[1])
 
-            idxs=np.random.randint(0,self.pt1.shape[0],size=self.sample)        
-            pts1=self.pt1[idxs,:]
-            pts2=self.pt2[idxs,:]
+            idxs=np.random.randint(0,self.kp1.shape[1],size=self.sample) 
+                  
+            
+            pts1=self.kp1[:,idxs]
+            pts2=self.kp2[:,idxs]
 
             #compute model parameter
             
             self.model.compute_parameter(pts1,pts2)
             
             #calculate the reprojection error
-            error=self.model.error2(self.pt1,self.pt2)
-            
+            error=self.model.error(self.kp1,self.kp2)
+
             if error is None:
                 continue
             
@@ -35,8 +44,8 @@ class RANSAC:
             
                     
             if (np.sum(mask)>np.sum(best_inlier)):
-                print(np.sum(mask))  
-                self.model.params=params
+                # print(np.sum(mask))  
+                best_params=self.model.params
                 best_inlier=mask
             # if (np.sum(mask)>T):
             #     break
@@ -45,6 +54,7 @@ class RANSAC:
 
             # print(inliers_pts1.size)
             i+=1
+        self.model.params=best_params
         
         return self.model,best_inlier,np.sum(error)
         # plt.show()
