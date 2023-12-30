09538eabb9d6e2ac9f6042688484b0e46a209c22
Sujan-PhdWork <104067836+Sujan-PhdWork@users.noreply.github.com>
2023-12-30 12:02:17 +0530

    normalization_and_ransac

--- extractor.py
+++ extractor.py
@@ -1,5 +1,15 @@
 import cv2
 import numpy as np
+from transformation import *
+from ransac import *
+
+
+
+def projected_map(kps,depth):
+    for p in kps:
+        u,v=map(lambda x: int(round(x)),p.pt)
+        Z=depth[v,u]
+        # print(u,v,Z)
 
 class Extractor():
     def __init__(self):
@@ -7,7 +17,7 @@ class Extractor():
         self.bf=cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last=None
 
-    def extract(self,img):
+    def extract(self,img,depth):
         #detection
         feats=cv2.goodFeaturesToTrack(np.mean(img,axis=2).astype(np.uint8),3000,qualityLevel=0.01,minDistance=3)
         
@@ -22,8 +32,33 @@ class Extractor():
             for m,n in matches:
                 if m.distance<0.75*n.distance:
                     kp1=kps[m.queryIdx].pt
+
+                    #projected 2d point to 3d
+                    u1,v1=map(lambda x: int(round(x)),kp1)
+                    Z1=depth[v1,u1]
+                    kp1=(u1,v1,Z1)
+
+
+                    #projected 2d point to 3d
                     kp2=self.last['kps'][m.trainIdx].pt
+                    u2,v2=map(lambda x: int(round(x)),kp2)
+                    Z2=depth[v2,u2]
+                    kp2=(u2,v2,Z2)
                     ret.append((kp1,kp2))
-            
+
+        
+
+        if len(ret)>0:
+        
+            ransac=RANSAC(ret,Transformation(),3,0.5,100)
+            model,inlier,error=ransac.solve()
+
+            ret=np.array(ret)
+
+            ret=ret[inlier]
+
+
+
+
         self.last={'kps':kps,'des':des}
         return ret
