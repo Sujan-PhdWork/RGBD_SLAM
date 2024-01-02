e25abe339fed0a2845ddbba5273bbd51e861edde
Sujan-PhdWork <104067836+Sujan-PhdWork@users.noreply.github.com>
2024-01-02 11:43:52 +0530

    Point and Map class added

---
+++ main.py
@@ -0,0 +1,100 @@
+import cv2
+import numpy as np
+from utils import *
+from frame import *
+import matplotlib.pyplot as plt
+import g2o
+from pointmap import Point,Map
+np.set_printoptions(suppress=True)
+# Process_flag=True
+
+mapp=Map()
+
+
+
+def process_img(img,depth):
+
+    frame=Frame(mapp,img,depth)
+    # frames.append(frame)
+
+    if frame.id==0:
+        return
+    
+    f1=mapp.frames[-1]
+    f2=mapp.frames[-2]
+    idx1,idx2,pose=match(f1,f2)
+
+    
+    if idx1 is None:
+        return
+    if idx2 is None:
+        return
+    
+    if pose is None:
+        return
+    
+    f1.pose=np.dot(pose,f2.pose)
+
+    # print(frames[-1].pts)
+    pt4ds=add_ones(f1.pts[idx1])
+    # print(pt4d.shape)
+    pt4ds=np.dot(f2.pose,pt4ds.T).T
+    # print(idx1)
+    # print(pt4ds.shape)
+
+    for i,p in enumerate(pt4ds):
+        pt=Point(mapp,p)
+        pt.add_observation(f1,idx1[i])
+        pt.add_observation(f2,idx2[i])
+    
+    # print(pt4d[:,0])
+    
+    
+
+    # print(frames[-1].pose)
+
+    for kp1,kp2 in zip(f1.pts[idx1],f2.pts[idx2]):
+        u1,v1,_=map(lambda x: int(round(x)),kp1)
+        u2,v2,_=map(lambda x: int(round(x)),kp2)
+
+        cv2.circle(img,(u1,v1),color=(0,255,0),radius=3)
+        cv2.line(img,(u1,v1),(u2,v2),color=(255,0,0))
+    disp(img,"RGB")
+
+    mapp.display()
+
+
+if __name__ == "__main__":
+    
+    depth_paths='../rgbd_dataset_freiburg1_xyz/depth.txt'
+    dlist=data(depth_paths)
+
+    rgb_paths='../rgbd_dataset_freiburg1_xyz/rgb.txt'
+    ilist=data(rgb_paths)
+
+    dataset_path='../rgbd_dataset_freiburg1_xyz/'
+    
+
+    for i in range(len(dlist)):
+
+        frame=cv2.imread(dataset_path+ilist[i])
+        depth=cv2.imread(dataset_path+dlist[i],0)
+        # print(frame.shape,depth.shape)
+
+
+        process_img(frame,depth)
+        # print(frame)
+
+        if frame is None:
+            print("End of frame")
+            break
+
+        
+        disp(depth,"Depth")
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+        
---
+++ pointmap.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pangolin
+import OpenGL.GL as gl
+from multiprocessing import Process, Queue
+
+
+class Map(object):
+    def __init__(self):
+        self.frames=[]
+        self.points=[]
+        self.state=None
+        self.q=Queue()
+
+        self.p=Process(target=self.viewer_thread,args=(self.q,))
+        self.p.demon=True
+        self.p.start()
+        
+    def viewer_thread(self,q):
+        self.viewer_init(640,480)
+        while not pangolin.ShouldQuit():
+            self.viewer_refresh(q)
+
+
+    def viewer_init(self,W,H):
+        pangolin.CreateWindowAndBind('Main', W, H)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
+        self.scam = pangolin.OpenGlRenderState(
+            pangolin.ProjectionMatrix(W, H, 420, 420, W//2, H//2, 0.2, 1000),
+            pangolin.ModelViewLookAt(0, -10, -2, 0, 0, -1, pangolin.AxisDirection.AxisX))
+        self.handler = pangolin.Handler3D(self.scam)
+
+        # Create Interactive View in window
+        self.dcam = pangolin.CreateDisplay()
+        self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+        self.dcam.SetHandler(self.handler)
+
+
+    def viewer_refresh(self,q):
+
+        if self.state is None or not q.empty():
+            self.state=q.get()
+        
+        ppts=np.array([d[:3,3] for d in self.state[0]])
+        spts=np.array(self.state[1])
+
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        self.dcam.Activate(self.scam)
+        
+
+
+        gl.glPointSize(10)
+        gl.glColor3f(1.0, 0.0, 0.0)
+        pangolin.DrawPoints(ppts)
+
+        gl.glPointSize(2)
+        gl.glColor3f(0.0, 1.0, 0.0)
+        # spts=np.array(self.state[1])
+
+        pangolin.DrawPoints(spts)
+        
+        pangolin.FinishFrame()
+
+
+
+
+    def display(self):
+        poses,pts=[],[]
+        for f in self.frames:
+            poses.append(f.pose)
+        for p in self.points:
+            pts.append(p.pt)
+        self.q.put((np.array(poses),np.array(pts)))
+
+class Point(object):
+    # apoint is a 3D point in the world
+    #each Point is observed in multiple frame
+    def __init__(self,mapp,loc):
+        self.pt=loc #state vector
+        self.frames=[]
+        self.idxs=[]
+
+        self.id=len(mapp.points)
+        mapp.points.append(self)
+
+    def add_observation(self,frame,idx):
+        self.frames.append(frame)
+        self.idxs.append(idx)
