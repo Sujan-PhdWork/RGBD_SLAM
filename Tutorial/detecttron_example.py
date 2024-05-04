c96db13ef9136988ba0bbffa07dc4635c5020033
sujan <sujankd20@iitk.ac.in>
2024-05-04 06:14:49 +0530

    video segmentation is done

--- Tutorial/detecttron_example.py
+++ Tutorial/detecttron_example.py
@@ -10,47 +10,64 @@ from detectron2.utils.visualizer import Visualizer,ColorMode
 from detectron2.data import MetadataCatalog
 
 
-imagePath = "demo.jpg"
-myNewImage = cv2.imread(imagePath)
-scale_precent = 30
-
-width = int(myNewImage.shape[1] * scale_precent / 100)
-height = int(myNewImage.shape[0] * scale_precent / 100)
-dim = (width, height)
-
-myNewImage = cv2.resize(myNewImage, dim, interpolation=cv2.INTER_AREA)
-# Panoptic Segmentation = Instance Segmentation + Semnatic Segmentation
-
 cfg_pan = get_cfg()
 cfg_pan.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x.yaml'))
 cfg_pan.MODEL.WEIGHTS = model_zoo.get_checkpoint_url('COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x.yaml')
-# cfg_pan.MODEL.DEVICE = "cpu" # if you have Cuda, dont need this line
-predictor= DefaultPredictor(cfg_pan)
 
-t=time.time()
-predictions=predictor(myNewImage)
-eps=time.time()-t
-print(eps)
-instances=predictions["instances"].to("cpu")
+def process_img(frame):
 
-# print(instances.)
-# print(predictions["pred_masks"])
+    global cfg_pan
 
-mask_image=instances.pred_masks.numpy()
+    width=frame.shape[1]
+    height=frame.shape[0]
+    # dim = (width, height)
 
-mask_image=np.sum(mask_image,axis=0)
-mask_image=mask_image.astype(np.uint8)
-# mask_image=mask_image
-print(mask_image.shape,myNewImage.shape)
+    myNewImage=cv2.resize()
+    myNewImage=frame
+
+    # cfg_pan.MODEL.DEVICE = "cpu" # if you have Cuda, dont need this line
+    predictor= DefaultPredictor(cfg_pan)
+
+    t=time.time()
+    predictions=predictor(myNewImage)
+    eps=time.time()-t
+    print(eps)
+    instances=predictions["instances"].to("cpu")
+
+    # print(instances.)
+    # print(predictions["pred_masks"])
+
+    mask_image=instances.pred_masks.numpy()
+
+    mask_image=np.sum(mask_image,axis=0)
+    mask_image=mask_image.astype(np.uint8)
+
+    cv2.imshow("img", myNewImage)
+    cv2.imshow("predict", mask_image*255)
 
 
 
-# viz = Visualizer (myNewImage[:, :,::-1], MetadataCatalog.get(cfg_pan.DATASETS.TRAIN[0]), instance_mode=ColorMode.IMAGE_BW)
 
-# output= viz.draw_instance_predictions(predictions["instances"].to("cpu"))
+vid = cv2.VideoCapture(0) 
 
-# img = output.get_image()[:, :, ::-1]
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
+    
+    process_img(frame)
 
-cv2.imshow("img", myNewImage)
-cv2.imshow("predict", mask_image*255)
-cv2.waitKey(0)
+    # Display the resulting frame 
+    # cv2.imshow('frame', frame) 
+      
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
